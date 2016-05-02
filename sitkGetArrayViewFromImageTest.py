2a6be592868fd52b2432ba4385ec69781a52ef0d
Hyun Jae Kang <hyunjae.kang@kitware.com>
2016-05-02 13:50:26 -0400

    ENH: Update the test code of sitkGetArrayViewFromImageTest.py.
    - Update the test code of sitkGetArrayViewFromImageTest.py to test the
      functions of GetArrayFromImage and GetImageFromArray.
    
    - Update the function of "test_processing_time" by adding the
      performance test of GetImageFromArray.
    
    - Added the following test function to check memory management of
      converted NumPy array view and sitk::Image view.

--- sitkGetArrayViewFromImageTest.py
+++ sitkGetArrayViewFromImageTest.py
@@ -40,12 +40,12 @@ class TestNumpySimpleITKMemoryviewInterface(unittest.TestCase):
 
     def _helper_check_sitk_to_numpy_type(self, sitkType, numpyType):
         image = sitk.Image( (9, 10), sitkType, 1 )
-        a = sitk.GetArrayViewFromImage( image )
+        a = sitk.GetArrayFromImage( image, arrayview = True)
         self.assertEqual( numpyType, a.dtype )
         self.assertEqual( (10, 9), a.shape )
 
     def test_type_to_numpy(self):
-        "try all sitk pixel type to convert to numpy"
+        "try all sitk pixel type to convert to NumPy array view"
 
         self._helper_check_sitk_to_numpy_type(sitk.sitkUInt8, np.uint8)
         self._helper_check_sitk_to_numpy_type(sitk.sitkUInt16, np.uint16)
@@ -59,8 +59,6 @@ class TestNumpySimpleITKMemoryviewInterface(unittest.TestCase):
             self._helper_check_sitk_to_numpy_type(sitk.sitkInt64, np.int64)
         self._helper_check_sitk_to_numpy_type(sitk.sitkFloat32, np.float32)
         self._helper_check_sitk_to_numpy_type(sitk.sitkFloat64, np.float64)
-        #self._helper_check_sitk_to_numpy_type(sitk.sitkComplexFloat32, np.complex64)
-        #self._helper_check_sitk_to_numpy_type(sitk.sitkComplexFloat64, np.complex128)
         self._helper_check_sitk_to_numpy_type(sitk.sitkVectorUInt8, np.uint8)
         self._helper_check_sitk_to_numpy_type(sitk.sitkVectorInt8, np.int8)
         self._helper_check_sitk_to_numpy_type(sitk.sitkVectorUInt16, np.uint16)
@@ -73,23 +71,19 @@ class TestNumpySimpleITKMemoryviewInterface(unittest.TestCase):
             self._helper_check_sitk_to_numpy_type(sitk.sitkVectorInt64, np.int64)
         self._helper_check_sitk_to_numpy_type(sitk.sitkVectorFloat32, np.float32)
         self._helper_check_sitk_to_numpy_type(sitk.sitkVectorFloat64, np.float64)
-        #self._helper_check_sitk_to_numpy_type(sitk.sitkLabelUInt8, np.uint8)
-        #self._helper_check_sitk_to_numpy_type(sitk.sitkLabelUInt16, np.uint16)
-        #self._helper_check_sitk_to_numpy_type(sitk.sitkLabelUInt32, np.uint32)
-        #self._helper_check_sitk_to_numpy_type(sitk.sitkLabelUInt64, np.uint64)
 
     def test_to_numpy_and_back(self):
-        """Test converting an image to numpy memoryview and back"""
+        """Test converting an image to NumPy array view and back"""
 
         img = sitk.GaussianSource( sitk.sitkFloat32,  [100,100], sigma=[10]*3, mean = [50,50] )
 
         h = sitk.Hash( img )
 
-        img = sitk.GetImageFromArray( sitk.GetArrayViewFromImage( img ) )
-        self.assertEqual( h, sitk.Hash( img ))
+        img2 = sitk.GetImageFromArray( sitk.GetArrayFromImage( img, arrayview = True), imageview = True)
+        self.assertEqual( h, sitk.Hash( img2 ))
 
     def test_vector_image_to_numpy(self):
-        """Test converting back and forth between numpy memoryview and SimpleITK
+        """Test converting back and forth between NumPy array view and SimpleITK
         images were the SimpleITK image has multiple componets and
         stored as a VectorImage."""
 
@@ -98,21 +92,21 @@ class TestNumpySimpleITKMemoryviewInterface(unittest.TestCase):
         img = sitk.PhysicalPointSource(sitk.sitkVectorFloat32, [3,4])
         h = sitk.Hash( img )
 
-        nda = sitk.GetArrayViewFromImage(img)
+        nda = sitk.GetArrayFromImage(img, arrayview = True)
 
         self.assertEqual(nda.shape, (4,3,2))
         self.assertEqual(nda[0,0].tolist(), [0,0])
         self.assertEqual(nda[2,1].tolist(), [1,2])
         self.assertEqual(nda[0,:,0].tolist(), [0,1,2])
 
-        img2 = sitk.GetImageFromArray( nda, isVector=True)
+        img2 = sitk.GetImageFromArray( nda, isVector=True, imageview = True)
         self.assertEqual( h, sitk.Hash(img2) )
 
         # check 3D
         img = sitk.PhysicalPointSource(sitk.sitkVectorFloat32, [3,4,5])
         h = sitk.Hash( img )
 
-        nda = sitk.GetArrayViewFromImage(img)
+        nda = sitk.GetArrayFromImage(img, arrayview = True)
 
         self.assertEqual(nda.shape, (5,4,3,3))
         self.assertEqual(nda[0,0,0].tolist(), [0,0,0])
@@ -120,7 +114,7 @@ class TestNumpySimpleITKMemoryviewInterface(unittest.TestCase):
         self.assertEqual(nda[0,:,1,1].tolist(), [0,1,2,3])
 
 
-        img2 = sitk.GetImageFromArray(nda)
+        img2 = sitk.GetImageFromArray(nda, imageview = True)
         self.assertEqual(img2.GetSize(), img.GetSize())
         self.assertEqual(img2.GetNumberOfComponentsPerPixel(), img.GetNumberOfComponentsPerPixel())
         self.assertEqual(h, sitk.Hash(img2))
@@ -135,9 +129,9 @@ class TestNumpySimpleITKMemoryviewInterface(unittest.TestCase):
           for i in range(sizeX):
               image[i, j] = j*sizeX + i
 
-      print(sitk.GetArrayViewFromImage(image))
+      print(sitk.GetArrayFromImage(image, arrayview = True))
 
-      self.assertEqual( type (sitk.GetArrayViewFromImage(image)),  np.ndarray )
+      self.assertEqual( type (sitk.GetArrayFromImage(image, arrayview = True)),  np.ndarray )
 
       # 3D image
       image = sitk.Image(sizeX, sizeY, sizeZ, sitk.sitkFloat32)
@@ -146,28 +140,46 @@ class TestNumpySimpleITKMemoryviewInterface(unittest.TestCase):
               for i in range(sizeX):
                   image[i, j, k] = (sizeY*k +j)*sizeX + i
 
-      print(sitk.GetArrayViewFromImage(image))
+      print(sitk.GetArrayFromImage(image, arrayview = True))
 
-      self.assertEqual( type (sitk.GetArrayViewFromImage(image)),  np.ndarray )
+      self.assertEqual( type (sitk.GetArrayFromImage(image, arrayview = True)),  np.ndarray )
 
     def test_processing_time(self):
       """Check the processing time the conversions from SimpleITK Image
          to numpy array (GetArrayFromImage) and
          numpy memoryview (GetArrayViewFromImage)."""
 
+      # Performance test for SimpleITK Image -> NumPy array
       img = sitk.GaussianSource( sitk.sitkFloat32,  [3000,3000], sigma=[10]*3, mean = [50,50] )
 
-      nparray_time_start =  dt.datetime.now()
-      nparray = sitk.GetArrayFromImage(img)
-      nparray_time_elapsed = dt.datetime.now() - nparray_time_start
-      print ("\nProcessing time of GetArrayFromImage    :: %.1f (us)"%nparray_time_elapsed.microseconds)
+      print("\nGet NumPy array from 3000x3000 SimpleITK Image")
+      nparray_time_start     =  dt.datetime.now()
+      nparray                = sitk.GetArrayFromImage(img)
+      nparray_time_elapsed   = dt.datetime.now() - nparray_time_start
+      print ("Processing time of GetArrayFromImage (Copy operation)  :: %.1f (us)"%nparray_time_elapsed.microseconds)
 
-      npview_time_start =  dt.datetime.now()
-      npview = sitk.GetArrayViewFromImage(img)
-      npview_time_elapsed = dt.datetime.now() - npview_time_start
-      print ("Processing time of GetArrayViewFromImage:: %.1f (us)"%npview_time_elapsed.microseconds)
+      npview_time_start      = dt.datetime.now()
+      npview                 = sitk.GetArrayFromImage(img, arrayview = True)
+      npview_time_elapsed    = dt.datetime.now() - npview_time_start
+      print ("Processing time of GetArrayFromImage (Array view)      :: %.1f (us)"%npview_time_elapsed.microseconds)
 
       self.assertTrue( nparray_time_elapsed > npview_time_elapsed)
+
+      # Performance test for NumPy array -> SimpleITK Image
+      Big_nparray = np.zeros((3000,3000), dtype = np.int64);
+
+      print("\nGet SimpleITK Image from 3000x3000 NumPy array")
+      time_start             = dt.datetime.now()
+      img_copy               = sitk.GetImageFromArray(Big_nparray)
+      time_elapsed_copy      = dt.datetime.now() - time_start
+      print ("Processing time of GetImageFromArray (Copy operation)  :: %.1f (us)"%time_elapsed_copy.microseconds)
+
+      time_start             =  dt.datetime.now()
+      imgview                = sitk.GetImageFromArray(Big_nparray, imageview = True)
+      time_elapsed_imageview = dt.datetime.now() - time_start
+      print ("Processing time of GetImageFromArray (Image view)      :: %.1f (us)"%time_elapsed_imageview.microseconds)
+
+      self.assertTrue( time_elapsed_copy > time_elapsed_imageview)
 
     def test_numpy_memoryview_writeable_flag(self):
       """Test the second argument (writeable) of the GetArrayViewFromImage function."""
@@ -177,8 +189,8 @@ class TestNumpySimpleITKMemoryviewInterface(unittest.TestCase):
           for i in range(sizeX):
               image[i, j] = j*sizeX + i
 
-      npviewReadOnly  = sitk.GetArrayViewFromImage(image, False)
-      npviewReadWrite = sitk.GetArrayViewFromImage(image, True)
+      npviewReadOnly  = sitk.GetArrayFromImage(image, arrayview = True, writeable = False)
+      npviewReadWrite = sitk.GetArrayFromImage(image, arrayview = True, writeable = True)
 
       image.SetPixel(0,0, newSimpleITKPixelValueInt32)
 
@@ -188,22 +200,23 @@ class TestNumpySimpleITKMemoryviewInterface(unittest.TestCase):
       try:
         npviewReadOnly[0][0] = newNumPyElementValueInt32
       except:
-        print ("We cannot modify an element value of the numpy memoryview", \
+        print ("We cannot modify an element value of the NumPy array view", \
                 "which was converted with ReadOnly mode.")
 
       npviewReadWrite[0][0] = newNumPyElementValueInt32
 
       self.assertEqual( image.GetPixel(0,0),newNumPyElementValueInt32)
 
-    def test_numpy_memoryview_deletions(self):
+    def test_NumPy_arrayview_deletion_sitkImage_1(self):
       # 2D image
       image = sitk.Image(sizeX, sizeY, sitk.sitkInt32)
       for j in range(sizeY):
           for i in range(sizeX):
               image[i, j] = j*sizeX + i
 
-      npview = sitk.GetArrayViewFromImage(image, True)
+      npview = sitk.GetArrayFromImage(image, arrayview = True, writeable = True)
       image.SetPixel(0,0, newSimpleITKPixelValueInt32)
+
       del image
 
       carr = np.array(npview, copy = False)
@@ -222,6 +235,40 @@ class TestNumpySimpleITKMemoryviewInterface(unittest.TestCase):
       self.assertEqual( rarr[0,0],newNumPyElementValueInt32)
       self.assertEqual( varr[0,0],np.int8(newNumPyElementValueInt32))
 
+    def test_NumPy_arrayview_deletion_sitkImage_2(self):
+      # 2D image
+      sizeX                                = 3
+      sizeY                                = 3
+      imgNewPixelValueInt32                = 30
+      imgSitkShallowCopyNewPixelValueInt32 = -50
+
+      img = sitk.Image(sizeX, sizeY, sitk.sitkInt32)
+      for j in range(sizeY):
+        for i in range(sizeX):
+          img[i, j] = j*sizeX + i
+
+      imgSitkShallowCopy = sitk.Image(img)
+
+      arr = sitk.GetArrayFromImage(img, arrayview = True)
+
+      # img and imgSitkShallCopy have different pixel buffers because of a deep copy
+      img.SetPixel(0,0,imgNewPixelValueInt32)
+      imgSitkShallowCopy.SetPixel(0,0,imgSitkShallowCopyNewPixelValueInt32)
+
+      del imgSitkShallowCopy
+
+      self.assertEqual( arr[0,0],imgNewPixelValueInt32)
+
+    def test_sitkImageView_deletions_1(self):
+      npArrayPixelValueInt32                = 30
+      nparray = np.zeros((3,3), dtype = np.int32);
+      nparray[0,0] = npArrayPixelValueInt32
+
+      img = sitk.GetImageFromArray(nparray, imageview = True)
+
+      del nparray
+
+      self.assertEqual(img.GetPixel(0,0), npArrayPixelValueInt32)
 
 if __name__ == '__main__':
     unittest.main()
